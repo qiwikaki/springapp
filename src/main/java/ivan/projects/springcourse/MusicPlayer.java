db438871ce3ddc15f134c96f7bcdb98dcdbfea78
Ivan <47557610+qiwikaki@users.noreply.github.com>
2022-10-25 15:34:19 +0300

    Annotation @Autowired-dependency injection ch.1

--- src/main/java/ivan/projects/springcourse/MusicPlayer.java
+++ src/main/java/ivan/projects/springcourse/MusicPlayer.java
@@ -1,37 +1,23 @@
 package ivan.projects.springcourse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MusicPlayer {
-    private Music music;
-    private String name;
-    private int volume;
 
-    public String getName() {
-        return name;
-    }
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-    public MusicPlayer(){}
-
-    public void setMusic(Music music) {
-        this.music = music;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
 
     //IoC
-    public void playMusic() {
-        System.out.println("Playing... " + music.getSong());
+    public String playMusic() {
+        return "Playing... " + classicalMusic.getSong();
+//        System.out.println("Playing... " + rockMusic.getSong());
     }
 }
--- src/main/java/ivan/projects/springcourse/TestSpring.java
+++ src/main/java/ivan/projects/springcourse/TestSpring.java
@@ -8,33 +8,13 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-//        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
-//        System.out.println(classicalMusic.getSong());
+//        MusicPlayer musicPlayer = context.getBean(MusicPlayer.class);
+//        musicPlayer.playMusic();
 
-        Music music =  context.getBean("rockMusic", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-        musicPlayer.playMusic();
+        Computer computer  = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
-        Music music2 =  context.getBean("classicalMusic", Music.class);
-        MusicPlayer classMusicPlayer = new MusicPlayer(music2);
-        classMusicPlayer.playMusic();
-        System.out.println("test");
         context.close();
-
-//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//
-//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-//        System.out.println(comparison);
-//
-//        System.out.println(firstMusicPlayer);
-//        System.out.println(secondMusicPlayer);
-//
-//        firstMusicPlayer.setVolume(18);
-//        System.out.println(firstMusicPlayer.getVolume());
-//        System.out.println(secondMusicPlayer.getVolume());
-//        System.out.println(musicPlayer.getName());
-//        System.out.println(musicPlayer.getVolume());
 
     }
 }
