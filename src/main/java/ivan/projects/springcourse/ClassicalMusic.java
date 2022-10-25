9801c09978964d83eec02c8725189c21c4bba38d
Ivan <47557610+qiwikaki@users.noreply.github.com>
2022-10-25 16:51:57 +0300

    Annotation @Scope,@Value-dependency injection ch.3

--- src/main/java/ivan/projects/springcourse/ClassicalMusic.java
+++ src/main/java/ivan/projects/springcourse/ClassicalMusic.java
@@ -1,8 +1,13 @@
 package ivan.projects.springcourse;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 @Component
+//@Scope("prototype")
 public class ClassicalMusic implements Music{
+    public void doMyInit(){
+        System.out.println("doMyIni");
+    }
 
     @Override
     public String getSong() {
--- src/main/java/ivan/projects/springcourse/TestSpring.java
+++ src/main/java/ivan/projects/springcourse/TestSpring.java
@@ -10,9 +10,15 @@ public class TestSpring {
 
 //        MusicPlayer musicPlayer = context.getBean(MusicPlayer.class);
 //        musicPlayer.playMusic();
+//        Computer computer  = context.getBean("computer", Computer.class);
+//        System.out.println(computer);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
-        Computer computer  = context.getBean("computer", Computer.class);
-        System.out.println(computer);
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
+        ClassicalMusic classicalMusic2 = context.getBean("classicalMusic", ClassicalMusic.class);
+        System.out.println(classicalMusic1 == classicalMusic2);
 
         context.close();
 
